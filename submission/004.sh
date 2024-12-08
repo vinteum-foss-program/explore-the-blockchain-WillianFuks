1ef23fcbcc61f8deb0336265eeb3b800dee84f44
WillianFuks <willian.fuks@gmail.com>
2024-12-08 19:49:18 -0300

    Fix question 004

--- submission/004.sh
+++ submission/004.sh
@@ -3,4 +3,4 @@
 # 0 is external chain, we could then have unbounded addresses or a specific one, in this case using 100th index (address at position 101)
 DESCRIPTOR_INFO=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)")
 DESCRIPTOR=$(echo "$DESCRIPTOR_INFO" | jq -r '.descriptor')
-bitcoin-cli deriveaddresses "$DESCRIPTOR"
+bitcoin-cli deriveaddresses "$DESCRIPTOR"| jq -r '.[0]'
